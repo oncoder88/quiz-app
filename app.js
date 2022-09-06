00f399b5b73de4653d3364ccfbefcbdc6d046785
oncoder88 <elensoroka88@gmail.com>
2022-09-06 21:08:01 +0100

    Get answer selection/deselection and counting the score done

--- app.js
+++ app.js
@@ -42,7 +42,9 @@ const quizData = [
   },
 ];
 
+const quiz = document.getElementById("quiz");
 const questionEl = document.getElementById("question");
+const answerEls = document.querySelectorAll(".answer");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
@@ -50,11 +52,15 @@ const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
+  deselectAnswers();
+
   const currentQuizData = quizData[currentQuiz];
+
   questionEl.innerText = currentQuizData.question;
   a_text.innerText = currentQuizData.a;
   b_text.innerText = currentQuizData.b;
@@ -62,14 +68,41 @@ function loadQuiz() {
   d_text.innerText = currentQuizData.d;
 }
 
+function getSelected() {
+  let answer = undefined;
+
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+
+  return answer;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
+}
+
 submitBtn.addEventListener("click", () => {
-    currentQuiz++;
-    if(currentQuiz < quizData.length) {
-        loadQuiz();
-    } else {
-        //TODO: 
-        alert("You finished! Get yourself an Orange Lemonade");
+  //check to see the answer
+
+  const answer = getSelected();
+
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
     }
 
-    loadQuiz();
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+    
+      <button onclick="location.reload()">Reload</button>`;
+    }
+  }
 });
