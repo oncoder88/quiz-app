0839172a232e9005415c631cc60bb2d78d380956
oncoder88 <elensoroka88@gmail.com>
2022-09-06 16:01:18 +0100

    Created a function loadQuiz

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ const quizData = [
     correct: "a",
   },
   {
-    question: "Which technical term is often used for data centres available over the Internet to many users?",
+    question:
+      "Which technical term is often used for data centres available over the Internet to many users?",
     a: "Sun computing",
     b: "Sky computing",
     c: "Cloud computing",
@@ -40,3 +41,35 @@ const quizData = [
     correct: "c",
   },
 ];
+
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  const currentQuizData = quizData[currentQuiz];
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+submitBtn.addEventListener("click", () => {
+    currentQuiz++;
+    if(currentQuiz < quizData.length) {
+        loadQuiz();
+    } else {
+        //TODO: 
+        alert("You finished! Get yourself an Orange Lemonade");
+    }
+
+    loadQuiz();
+});
