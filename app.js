95c3b063c0e272a2073167d555030e037de57afc
oncoder88 <elensoroka88@gmail.com>
2022-09-06 13:58:51 +0100

    Created quiz body data (5 questions, 4 options))

---
+++ app.js
@@ -0,0 +1,42 @@
+const quizData = [
+  {
+    question: "Which one of the following is a programming language?",
+    a: "HTTP",
+    b: "HTML",
+    c: "HPML",
+    d: "FTP",
+    correct: "b",
+  },
+  {
+    question: "Which of the following is not an operating system?",
+    a: "DOS",
+    b: "Mac",
+    c: "C",
+    d: "Linux",
+    correct: "c",
+  },
+  {
+    question: "Total number of oceans in the World is:",
+    a: "3",
+    b: "5",
+    c: "7",
+    d: "12",
+    correct: "b",
+  },
+  {
+    question: "What does IT stand for?",
+    a: "Information Technology",
+    b: "International Technology",
+    c: "Internet Technology",
+    d: "Intelligent Technology",
+    correct: "a",
+  },
+  {
+    question: "Which technical term is often used for data centres available over the Internet to many users?",
+    a: "Sun computing",
+    b: "Sky computing",
+    c: "Cloud computing",
+    d: "Net computing",
+    correct: "c",
+  },
+];
